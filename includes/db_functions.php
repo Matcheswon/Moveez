eb8b0b3f36ac6801762ede54e2d557458af4da56
Matcheswon <matthew.johnson04@gmail.com>
2014-05-07 20:16:03 -0500

    added ajax

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -1,19 +1,29 @@
 <?php
-if($_POST)
-{
-	$title = $_POST['title'];
-	$rating = $_POST['rating'];
 
+
+	
+    header("Content-type: application/json");
     //mysql_connect(host, username, password);
 	$con = mysql_connect("localhost", "root", "") or die(mysql_error() );
 	
 	//connect to database
 	mysql_select_db("moveez") or die( mysql_error() );
     
+    if(isset($_POST['create']) && $_POST['create'])
+    {    
+    $title = $_POST['title'];
+    $rating = $_POST['rating'];
     //insert title and rating into database
     mysql_query( "INSERT INTO movies (title, rating) VALUES ('$title', '$rating')" ) or die( mysql_error() );
+    }
+    
+    if(isset($_POST['delete']) && $_POST['delete'])
+    {
+        $id = $_POST['id'];
+        mysql_query("DELETE FROM movies WHERE id='$id'") or die(msql_error());
+    }
 
-    $result = mysql_query( "SELECT * FROM movies" ) or die( mysql_error() );
+    $result = mysql_query( "SELECT id, title, rating FROM movies" ) or die( mysql_error() );
 
     $rows = array();
     
@@ -25,4 +35,3 @@ if($_POST)
     echo json_encode($rows);
 
     mysql_close($con);
-}
